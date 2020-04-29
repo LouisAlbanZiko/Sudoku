a642e7cd1f61ead55a72aa89674bea6f9ef6b767
peTheProgrammer <louisalbanziko1610@gmail.com>
2020-04-29 10:04:50 -0700

    Added binary files for both 64 and 32 bit.

---
+++ cursor_windows/src/cursor_windows.cpp
@@ -0,0 +1,33 @@
+#include <windows.h>
+
+extern "C" {
+
+	__declspec(dllexport)
+		void setCursorPos(const short x, const short y) {
+			SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), { x, y });
+		}
+
+	__declspec(dllexport)
+		COORD getCursorPosition() {
+			CONSOLE_SCREEN_BUFFER_INFO info;
+			GetConsoleScreenBufferInfo(GetStdHandle(STD_OUTPUT_HANDLE), &info);
+			return info.dwCursorPosition;
+		}
+
+	__declspec(dllexport)
+		int getCursorX() {
+			return getCursorPosition().X;
+		}
+
+	__declspec(dllexport)
+		int getCursorY() {
+			return getCursorPosition().Y;
+		}
+
+	__declspec(dllexport)
+		void writeConsole(const char* string, const int length) {
+			char* buffer = new char[length];
+			WriteConsole(GetStdHandle(STD_OUTPUT_HANDLE), string, length, nullptr, NULL);
+		}
+
+} // extern "C"
